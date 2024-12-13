cf47b7c739f03c36757311ef442440b900732ba3
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-12-13 15:21:12 +0600

    responsivness

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -21,7 +21,7 @@ const ProductPage = ({ onAddToCart }) => {
 
   return (
     <div className="container mx-auto p-6">
-      <div className="grid grid-cols-2 gap-5 items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
         {/* Left Side */}
         <div className="left-side">
           <img
